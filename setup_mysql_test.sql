51c4991f57d509769e3125db5eb7780c84c8fe94
gabriel <gabaamazing21@gmail.com>
2024-09-27 02:36:23 +0100

    test mysql deployment

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,13 +1,13 @@
--- Create the database if it doesn't exist
+-- Create the test database if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- Create the user if it doesn't exist
+-- Create the test user if it doesn't already exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
--- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+-- Grant all privileges on the hbnb_test_db database to the hbnb_test user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
--- Grant SELECT privilege on performance_schema to the hbnb_dev user
+-- Grant SELECT privilege on performance_schema to the hbnb_test user
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 -- Apply the privilege changes
