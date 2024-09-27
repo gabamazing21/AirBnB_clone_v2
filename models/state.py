04af3f2a4f5e540a336acc00014279bbe54c4ca4
gabriel <gabaamazing21@gmail.com>
2024-09-27 20:30:00 +0100

    add lots cities and state to the db storage

--- models/state.py
+++ models/state.py
@@ -13,7 +13,6 @@ class State(BaseModel, Base):
     """State class"""
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
     @property
     def cities(self):
@@ -26,3 +25,5 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
+        else:
+            cities = relationship("City", backref="state", cascade="all, delete-orphan")
