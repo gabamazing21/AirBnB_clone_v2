b29930598b829f58a1b95ac2afccf71062e800d2
gabriel <gabaamazing21@gmail.com>
2024-09-27 02:21:47 +0100

    okay

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,14 @@
-
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd' ;
-GRANT ALL PRIVILEGES ON  hbnb_dev_db TO  'hbnb_dev'@'localhost';
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grant SELECT privilege on performance_schema to hbnb_dev user
+-- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to the hbnb_dev user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Apply the privilege changes
 FLUSH PRIVILEGES;
